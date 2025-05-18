586fa22aa1eadb5e83863a584f8d88805e27d36c
heimsharon <heimsharon@hotmail.com>
2025-05-17 21:00:10 -0400

    first commit

---
+++ server/src/routes/api/questionRoutes.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+const router = express.Router();
+import { getRandomQuestions } from '../../controllers/questionController.js';
+
+router.route('/random').get(getRandomQuestions);
+
+export default router;
